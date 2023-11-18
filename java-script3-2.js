dd29b44a5e0978c4b5ccdb5447ced2fc4b827d40
lory <lory608@gmail.com>
2023-11-18 15:30:35 +0100

    modifica esercizio banca

--- java-script3-2.js
+++ java-script3-2.js
@@ -1,42 +1,50 @@
-let soldi= 3000
 
-    let saluto
+//esercizio banca
 
-    while(saluto!= 3){
-        saluto=parseInt(prompt("salve,prema 1 per bonifico,prema 2 per prelievo, premi 3 per uscire"))
-        let soldipagati= parseInt(prompt("inserisci pagamento"))
 
-        switch(saluto){
+let soldi=3000
 
-            case 1:
-                if(soldi-soldipagati>=0){
-                    let risposta= prompt("inserisci il tuo IBAN")
-                    soldi=soldi-soldipagati;
 
-                    alert("adesso hai"+ ""+soldi)
+
+alert("salve e benvenuto in banca")
+
+let saluto
+
+while(saluto != 3){
+     saluto= parseInt(prompt("prema 1 per bonifico, prema 2 per prelievo, prema 3 per uscire"))
+    
+
+    switch(saluto){
+
+        case 1: let soldispesi=parseInt(prompt("immetti la cifra"))
+                 if(soldispesi<=soldi){
+                    parseInt(prompt("immetti Iban"))
+                    soldi=soldi-soldispesi
+                    alert("adesso hai"+" "+soldi+" "+"euro")
+                 }
+                 else{
+                    alert("mi dispiace ma non hai abbastanza soldi")
+                 }
+                
+                 break;
+
+                 
+        case 2: let soldiusati=parseInt(prompt("immetti la cifra"))
+                if(soldiusati<=soldi){
+                    alert("pagamento andato a buon fine")
+                    soldi=soldi-soldiusati
+                    alert("adesso hai"+" "+soldi+" "+"euro")
                 }
                 else{
-                    alert("soldi insufficienti")
+                    alert("mi dispiace ma non hai abbastanza soldi")
                 }
-
+                
                 break;
 
-            case 2:
-                if(soldi-soldipagati>=0){
-                    soldi=soldi-soldipagati;
-
-                    alert("adesso hai"+""+soldi)
-                }
-
-                else{
-                    alert("soldi insufficienti")
-                }
-
-            case 3:
-                alert("arrivederci")
-                break;
-        }
-
-        
-
+        case 3: alert("arrivederci e grazie")
+                 
     }
+}
+
+    let uscita= alert("sei uscito dal loop")
+        
